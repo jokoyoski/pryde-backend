e09093734ee3846d4775c131866a791356e20a79
Oduntan David <davidoduntan1@gmail.com>
2026-06-12 20:27:28 +0100

    vehicle entity

--- Pryde.Domain/Entities/Vehicle.cs
+++ Pryde.Domain/Entities/Vehicle.cs
@@ -8,5 +8,18 @@ namespace Pryde.Domain.Entities
 {
     public class Vehicle
     {
+        public Guid UserId { get; set; }
+
+        public string LicensePlateNumber { get; set; } = string.Empty;
+
+        public string VehicleImageUrl { get; set; } = string.Empty;
+
+        public int Capacity { get; set; }
+
+        public string? InsuranceDocumentUrl { get; set; }
+
+        public DateTime? InsuranceExpiryDate { get; set; }
+
+        public User User { get; set; } = null!;
     }
 }
