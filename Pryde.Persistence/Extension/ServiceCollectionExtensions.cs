a5fa4cc808bc8106b08bdd854a1813106782c685
Oduntan David <davidoduntan1@gmail.com>
2026-06-15 16:24:58 +0100

    added dependency injection extension

---
+++ Pryde.Persistence/Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryde.Persistence.Extension
+{
+    internal class ServiceCollectionExtensions
+    {
+    }
+}
