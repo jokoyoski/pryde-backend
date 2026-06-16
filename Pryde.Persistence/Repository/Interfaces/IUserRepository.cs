a50cd7f2ecdc9baef42e78ba3c6c5f122b4b8548
Oduntan David <davidoduntan1@gmail.com>
2026-06-16 15:41:42 +0100

    IuserRepo

---
+++ Pryde.Persistence/Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryde.Persistence.Repository.Interfaces
+{
+    internal class IUserRepository
+    {
+    }
+}
